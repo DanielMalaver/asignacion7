a54412c990a81367a65a7d8d7a01f2e319ef0b00
Daniel_Malaver <malaverdaniel22@gmail.com>
2021-03-28 00:57:22 -0400

    Actualización del archivo asignatura7.py

--- asignacion7.py
+++ asignacion7.py
@@ -1,19 +1,34 @@
 import requests
 from pprint import pprint
+import json
+import time
 
-response = requests.post(
-    'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token',
-    headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
-response.raise_for_status()
-payload=response.json()
-pprint(payload)
+while(1):
+    response = requests.post(
+        'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token',
+        headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
+    response.raise_for_status()
+    payload=response.json()
+    pprint(payload)
 
-response2 = requests.get(
-    'https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device',
-    headers={'X-Auth-Token':payload['Token']})
-payload2 = response2.json()['response']
-Output = []
-for i in range(len(payload2)):
-    Output.append([payload2[i]['family'],payload2[i]['hostname'],payload2[i]['managementIpAddress'],payload2[i]['lastUpdated'],payload2[i]['reachabilityStatus']])
-pprint(Output)
+    response2 = requests.get(
+        'https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device',
+        headers={'X-Auth-Token':payload['Token']})
+    payload2 = response2.json()['response']
+    Output = []
+    for i in range(len(payload2)):
+        Output.append([payload2[i]['family'],payload2[i]['hostname'],payload2[i]['managementIpAddress'],payload2[i]['lastUpdated'],payload2[i]['reachabilityStatus']])
+    pprint(Output)
 
+    data = {}
+    data['Equipos'] = []
+
+    for i in range(len(payload2)):
+        data['Equipos'].append({
+        'family': payload2[i]['family'],
+        'status': payload2[i]['reachabilityStatus']})
+
+    with open('list.json', 'w') as file:
+        json.dump(data, file, indent=4)
+    print("Proxima petición en 5 minutos")
+    time.sleep(60*5)
